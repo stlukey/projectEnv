052765b0804d6c13fd6909c3a602132d3712f7e6
Luke Southam <luke@devthe.com>
2012-11-25 18:49:33 +0000

    added more dependancys to install.sh

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-sudo apt-get install make
+sudo apt-get install make npm python-pip
 sudo npm install -g less
 sudo npm install -g coffee-script
 sudo pip install coffeescript
